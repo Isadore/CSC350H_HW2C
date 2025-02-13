731243a289975e39790d1dfdc1e0c1f20781ad5f
Isadore <remyrascal@live.com>
2025-02-13 01:31:43 -0500

    created menu

--- HW-2C/Program.cs
+++ HW-2C/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("**************");
+Console.WriteLine("Menu:");
+Console.WriteLine("1 - New Game");
+Console.WriteLine("2 - Load Game");
+Console.WriteLine("3 - Options");
+Console.WriteLine("4 - Quit");
+Console.WriteLine("**************");
